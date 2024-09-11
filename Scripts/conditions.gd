9365130fdf773b599b1cbbc63bf4303f27bd0629
I Exist <145796728+IExistAsAConstruct@users.noreply.github.com>
2024-09-11 15:27:26 +0800

    Disabled icon removal when two icons are left

--- Scripts/conditions.gd
+++ Scripts/conditions.gd
@@ -4,15 +4,7 @@ extends Control
 @onready var clock:Node = get_parent().get_node("Clock")
 signal second_hand_toggled(toggled_on:bool)
 signal minute_hand_toggled(toggled_on:bool)
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
+var iconsActive:int = 8
 
 func _on_minute_hand_check_toggled(toggled_on: bool) -> void:
 	if not toggled_on and secondHand.button_pressed:
@@ -26,3 +18,49 @@ func _on_second_hand_check_toggled(toggled_on: bool) -> void:
 		minuteHand.button_pressed = true
 		minute_hand_toggled.emit(true)
 	second_hand_toggled.emit(toggled_on)
+
+func change_active_icon_count(toggle_on: bool) -> void:
+	if toggle_on:
+		iconsActive += 1
+	else:
+		iconsActive -= 1
+	if iconsActive == 2:
+		for child in get_children():
+			if child is CheckBox:
+				if child.button_pressed:
+					child.disabled = true
+	else:
+		for child in get_children():
+			if child is CheckBox:
+				if child.button_pressed:
+					child.disabled = false
+
+func _on_fight_check_toggled(toggled_on: bool) -> void:
+	change_active_icon_count(toggled_on)
+
+
+func _on_magic_check_toggled(toggled_on: bool) -> void:
+	change_active_icon_count(toggled_on)
+
+
+func _on_defend_check_toggled(toggled_on: bool) -> void:
+	change_active_icon_count(toggled_on)
+
+func _on_item_check_toggled(toggled_on: bool) -> void:
+	change_active_icon_count(toggled_on)
+
+
+func _on_flee_check_toggled(toggled_on: bool) -> void:
+	change_active_icon_count(toggled_on)
+
+
+func _on_temporal_ability_check_toggled(toggled_on: bool) -> void:
+	change_active_icon_count(toggled_on)
+
+
+func _on_party_check_toggled(toggled_on: bool) -> void:
+	change_active_icon_count(toggled_on)
+
+
+func _on_tactics_check_toggled(toggled_on: bool) -> void:
+	change_active_icon_count(toggled_on)
