6e12c5dcff3ab2f6d565a0a4a22550eac0891eb6
I Exist <145796728+IExistAsAConstruct@users.noreply.github.com>
2024-09-29 22:50:02 +0800

    Moved code to other script, fixed hand movement
    Code that managed how time ran heavily modified to use a Timer properly and moved to its own script.
    
    Hand movement modified to be smoother and more consistent.
    
    Digital clock text changed to be dependent on control code instead of being integrated with control code.

--- Scripts/pocketwatch.gd
+++ Scripts/pocketwatch.gd
@@ -1,12 +1,13 @@
 extends Node2D
 var hourRotation:float = 0.0
 var minuteRotation:float = 0.0
+var secondRotation:float = 0.0
 var trueHours:float = 0.0
 @onready var root:Node = get_parent().get_parent()
 const hourRadians:float = PI/6
 const minuteRadians:float = PI/30
+const secondRadians:float = PI/30
 const SMOOTHNESS:float = 5.0
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,10 +25,14 @@ func _process(delta: float) -> void:
 	
 	var target_hour_rotation = trueHours * hourRadians
 	var target_minute_rotation = (minute + second / 60.0) * minuteRadians
+	var target_second_rotation = second * secondRadians
+	
 	#hourRotation = hourRadians * trueHours
 	#minuteRotation = minuteRadians * minute
 	
 	hourRotation = lerp_angle(hourRotation, target_hour_rotation, delta * secondInterval + 0.1)
 	minuteRotation = lerp_angle(minuteRotation, target_minute_rotation, delta * secondInterval + 0.1)
-	get_node("HourHand").rotation = hourRotation
-	get_node("MinuteHand").rotation = minuteRotation
+	secondRotation = lerp_angle(secondRotation, target_second_rotation, delta * secondInterval + 0.1)
+	get_node("HourHand").rotation = hourRotation + hourRadians
+	get_node("MinuteHand").rotation = minuteRotation + hourRadians
+	get_node("SecondHand").rotation = secondRotation + hourRadians
---
+++ Scripts/time_control.gd
@@ -0,0 +1,55 @@
+extends Control
+@export var minute:int = 0
+@export var hour:int = 0
+@export var secondInterval:int = 1
+var second:int = 0
+var trueHour:int = 0
+
+var timer:Timer
+var clockTicking: bool = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	trueHour = hour
+	timer = Timer.new()
+	add_child(timer)
+	timer.set_wait_time(secondInterval/60.0)
+	timer.set_one_shot(false)
+	timer.start()
+	
+	timer.timeout.connect(_on_timer_timeout)
+
+func _on_timer_timeout() -> void:
+	second += 1
+	
+	if second >= 60:
+		second = 0
+		minute += 1
+	
+	if minute >= 60:
+		minute = 0
+		hour += 1
+
+	if hour >= 24:
+		hour = 0
+	trueHour = hour
+
+func _on_activate_time_toggled(toggled_on: bool) -> void:
+	timer.set_paused(!toggled_on)
+
+func get_hour() -> int:
+	return trueHour
+
+func get_minute() -> int:
+	return minute
+	
+func get_second() -> int:
+	return second
+
+func get_second_interval() -> int:
+	return secondInterval
+
+
+func _on_interval_set_value_changed(value: float) -> void:
+	secondInterval = value
+	timer.set_wait_time(secondInterval/60.0)
